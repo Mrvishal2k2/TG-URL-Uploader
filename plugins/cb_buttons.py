b52eeaf64615cef24ad88ef94cec5f34bbf75db3
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2020-11-04 18:10:58 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -14,7 +14,7 @@ import os
 import shutil
 import subprocess
 import time
-from pyrogram.client.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
